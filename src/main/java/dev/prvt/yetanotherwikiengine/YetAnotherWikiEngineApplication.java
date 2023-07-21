3f194924bff3ca9db93bff19cd369c70ba53bf74
Seonghyeon Kim <kshshkim@users.noreply.github.com>
2023-07-21 11:43:49 +0900

    initial commit

---
+++ src/main/java/dev/prvt/yetanotherwikiengine/YetAnotherWikiEngineApplication.java
@@ -0,0 +1,13 @@
+package dev.prvt.yetanotherwikiengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YetAnotherWikiEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YetAnotherWikiEngineApplication.class, args);
+    }
+
+}
