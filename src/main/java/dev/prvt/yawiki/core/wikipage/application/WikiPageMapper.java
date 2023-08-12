4088d83474c939ec5c36669d1a3d0251b2b74b38
Seonghyeon Kim <kshshkim@users.noreply.github.com>
2023-08-12 18:02:21 +0900

    feature: WikiPageMapper mapFrom Revision&Contributor to RevisionData

--- src/main/java/dev/prvt/yawiki/core/wikipage/application/WikiPageMapper.java
+++ src/main/java/dev/prvt/yawiki/core/wikipage/application/WikiPageMapper.java
@@ -1,12 +1,37 @@
 package dev.prvt.yawiki.core.wikipage.application;
 
+import dev.prvt.yawiki.core.contributor.domain.Contributor;
+import dev.prvt.yawiki.core.contributor.domain.ContributorState;
+import dev.prvt.yawiki.core.wikipage.application.dto.RevisionData;
 import dev.prvt.yawiki.core.wikipage.application.dto.WikiPageDataForUpdate;
+import dev.prvt.yawiki.core.wikipage.domain.model.Revision;
 import dev.prvt.yawiki.core.wikipage.domain.model.WikiPage;
 import org.springframework.stereotype.Component;
+
+import java.util.Map;
+import java.util.UUID;
 
 @Component
 public class WikiPageMapper {
     public WikiPageDataForUpdate mapFrom(WikiPage wikiPage) {
         return new WikiPageDataForUpdate(wikiPage.getTitle(), wikiPage.getContent(), wikiPage.getVersionToken());
+    }
+
+    private String extractContributorName(Contributor contributor) {
+        if (contributor == null) {
+            return "NULL";
+        }
+        return contributor.getName();
+    }
+
+    public RevisionData mapFrom(Revision revision, Map<UUID, Contributor> contributorMap) {
+        String contributorName = extractContributorName(contributorMap.get(revision.getContributorId()));
+
+        return new RevisionData(
+                revision.getRevVersion(),
+                revision.getDiff(),
+                contributorName,
+                revision.getComment()
+        );
     }
 }
--- src/test/java/dev/prvt/yawiki/core/wikipage/application/WikiPageMapperTest.java
+++ src/test/java/dev/prvt/yawiki/core/wikipage/application/WikiPageMapperTest.java
@@ -1,20 +1,30 @@
 package dev.prvt.yawiki.core.wikipage.application;
 
+import dev.prvt.yawiki.core.contributor.domain.Contributor;
+import dev.prvt.yawiki.core.contributor.domain.ContributorState;
+import dev.prvt.yawiki.core.contributor.domain.MemberContributor;
+import dev.prvt.yawiki.core.wikipage.application.dto.RevisionData;
 import dev.prvt.yawiki.core.wikipage.application.dto.WikiPageDataForUpdate;
+import dev.prvt.yawiki.core.wikipage.domain.model.Revision;
 import dev.prvt.yawiki.core.wikipage.domain.model.WikiPage;
+import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Field;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
-import static dev.prvt.yawiki.Fixture.randString;
-import static dev.prvt.yawiki.Fixture.updateWikiPageRandomly;
+import static dev.prvt.yawiki.Fixture.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.tuple;
 
 class WikiPageMapperTest {
     private WikiPageMapper wikiPageMapper = new WikiPageMapper();
+
     @Test
-    void mapFrom_new_WikiPage() {
+    void mapFrom_WikiPage_new_WikiPage() {
         WikiPage wikiPage = WikiPage.create(randString());
         WikiPageDataForUpdate mapped = wikiPageMapper.mapFrom(wikiPage);
 
@@ -29,7 +39,7 @@ class WikiPageMapperTest {
     }
 
     @Test
-    void mapFrom_existing_WikiPage() {
+    void mapFrom_WikiPage_existing_WikiPage() {
         WikiPage wikiPage = WikiPage.create(randString());
         updateWikiPageRandomly(wikiPage);
         WikiPageDataForUpdate mapped = wikiPageMapper.mapFrom(wikiPage);
@@ -40,5 +50,36 @@ class WikiPageMapperTest {
         assertThat(List.of(mapped.title(), mapped.content(), mapped.versionToken()))
                 .doesNotContainNull()
                 .doesNotContain("");
+    }
+
+    @Test
+    void mapFrom_Revision_Map_Contributor_null() {
+        Revision givenRev = aRevision().build();
+        Map<UUID, Contributor> contributorMap = new HashMap<>();
+
+        // when
+        RevisionData revisionData = wikiPageMapper.mapFrom(givenRev, contributorMap);
+
+        // then
+        assertThat(revisionData.contributorName())
+                .isEqualTo("NULL");
+    }
+
+    @Test
+    void mapFrom_Revision_Map_Contributor_redacted() {
+        Revision givenRev = aRevision().build();
+        MemberContributor givenContributor = aMemberContributor()
+                .id(givenRev.getContributorId())
+                .build();
+
+        Map<UUID, Contributor> contributorMap = new HashMap<>();
+        contributorMap.put(givenRev.getContributorId(), givenContributor);
+
+        // when
+        RevisionData revisionData = wikiPageMapper.mapFrom(givenRev, contributorMap);
+
+        // then
+        assertThat(revisionData.contributorName())
+                .isEqualTo(givenContributor.getName());
     }
 }
