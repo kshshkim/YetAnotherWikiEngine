a1fa9ccee4f9cd6f5e2a8981042d45cbb67fe032
Seonghyeon Kim <kshshkim@users.noreply.github.com>
2023-09-27 15:16:42 +0900

    added: MemberRepository.java, MemberRepositoryTest.java

---
+++ src/main/java/dev/prvt/yawiki/core/member/domain/MemberRepository.java
@@ -0,0 +1,12 @@
+package dev.prvt.yawiki.core.member.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, UUID> {
+    Optional<Member> findByUsername(String username);
+}
---
+++ src/test/java/dev/prvt/yawiki/core/member/domain/MemberRepositoryTest.java
@@ -0,0 +1,46 @@
+package dev.prvt.yawiki.core.member.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+import java.util.Optional;
+import java.util.UUID;
+
+import static dev.prvt.yawiki.Fixture.randString;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class MemberRepositoryTest {
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Autowired
+    EntityManager em;
+
+    @Autowired
+    PasswordHasher passwordHasher;
+
+    Member givenMember;
+    @BeforeEach
+    void init() {
+        givenMember = Member.create(UUID.randomUUID(), randString(), randString(), passwordHasher);
+        memberRepository.save(givenMember);
+        em.flush();
+        em.clear();
+    }
+
+    @Test
+    @Transactional
+    void findByUsername() {
+        Optional<Member> found = memberRepository.findByUsername(givenMember.getUsername());
+        assertThat(found)
+                .isNotEmpty();
+        Member foundMember = found.orElseThrow();
+        assertThat(foundMember.getId())
+                .isEqualTo(givenMember.getId());
+    }
+}
