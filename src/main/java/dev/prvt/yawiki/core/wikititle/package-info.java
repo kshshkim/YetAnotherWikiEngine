ee224553e15a20eab3939b612c86f96ebfb2c0b0
Seonghyeon Kim <kshshkim@users.noreply.github.com>
2023-12-02 18:16:19 +0900

    init: 제목 존재 여부 판정 인터페이스 선언
    - doc: package-info.java 참조 방향 기입
    
    ref: #1

--- src/main/java/dev/prvt/yawiki/core/wikititle/package-info.java
+++ src/main/java/dev/prvt/yawiki/core/wikititle/package-info.java
@@ -1,4 +1,13 @@
 /**
  * 문서 제목의 존재 여부 판별, 제목 변경 내역, 캐싱 등에 관련된 패키지
+ * <p>
+ * 참조 관계<br>
+ * cache -> history<br>
+ * cache -> existencechecker
+ * </p>
+ * <p>
+ * 외부에서 사용되는 인터페이스<br>
+ * {@link dev.prvt.yawiki.core.wikititle.existence.WikiPageTitleExistenceChecker}
+ * </p>
  */
 package dev.prvt.yawiki.core.wikititle;
