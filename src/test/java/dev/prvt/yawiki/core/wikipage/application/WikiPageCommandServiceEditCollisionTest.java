e72055fab49b2515d19453a6783d3e14dde136a6
Seonghyeon Kim <kshshkim@users.noreply.github.com>
2023-12-27 15:48:07 +0900

    test refactor: 트랜잭션 시점 편집 충돌 테스트 리팩터
    - 제거가 예정된 proclaimUpdate, Query 서비스로 분리가 예정된 WikiPageDataForUpdate 관련 의존성 제거
    - 테스트 케이스 보완
    - 클린업 코드 제거
    - 불필요한 전역 변수 제거

--- src/test/java/dev/prvt/yawiki/core/wikipage/application/WikiPageCommandServiceEditCollisionTest.java
+++ src/test/java/dev/prvt/yawiki/core/wikipage/application/WikiPageCommandServiceEditCollisionTest.java
@@ -1,13 +1,12 @@
 package dev.prvt.yawiki.core.wikipage.application;
 
 
-import dev.prvt.yawiki.core.wikipage.application.dto.WikiPageDataForUpdate;
 import dev.prvt.yawiki.core.wikipage.domain.model.Namespace;
+import dev.prvt.yawiki.core.wikipage.domain.model.WikiPage;
 import dev.prvt.yawiki.core.wikipage.domain.model.WikiPageTitle;
-import dev.prvt.yawiki.core.wikipage.domain.validator.WikiPageCommandPermissionValidator;
+import dev.prvt.yawiki.core.wikipage.domain.repository.WikiPageRepository;
 import lombok.Builder;
 import lombok.Getter;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,29 +14,22 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.dao.CannotAcquireLockException;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.transaction.support.TransactionTemplate;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import java.util.HashSet;
+import java.util.Set;
 import java.util.UUID;
 
 import static dev.prvt.yawiki.fixture.Fixture.randString;
 import static org.assertj.core.api.Assertions.assertThat;
 
+/**
+ * <p>클린업 코드가 존재하지 않으며, 테스트 DB에 관련 데이터가 커밋됨. 문서 제목, 편집자 ID 등은 무작위 생성되어 반복 수행해도 문제가 없음.</p>
+ */
 @ExtendWith(MockitoExtension.class)
 @SpringBootTest
 public class WikiPageCommandServiceEditCollisionTest {
-
-    @MockBean
-    ApplicationEventPublisher applicationEventPublisher;
-
-    @MockBean
-    WikiPageCommandPermissionValidator wikiPageCommandPermissionValidator;
 
     @Autowired
     WikiPageCommandService wikiPageCommandService;
@@ -45,57 +37,67 @@ public class WikiPageCommandServiceEditCollisionTest {
     @Autowired
     TransactionTemplate transactionTemplate;
 
-    @PersistenceContext
-    EntityManager em;
+    @Autowired
+    WikiPageRepository wikiPageRepository;
 
-    WikiPageTitle givenTitle;
-
-    @BeforeEach
-    void init() {
-        givenTitle = new WikiPageTitle(randString(), Namespace.NORMAL);
-        wikiPageCommandService.create(UUID.randomUUID(), givenTitle);
-        WikiPageDataForUpdate wikiPageDataForUpdate = wikiPageCommandService.proclaimUpdate(UUID.randomUUID(), givenTitle);
-        wikiPageCommandService.commitUpdate(UUID.randomUUID(), givenTitle, randString(), wikiPageDataForUpdate.versionToken(), randString(), new HashSet<>());
+    private WikiPageTitle normalWikiPageTitle(String title) {
+        return new WikiPageTitle(title, Namespace.NORMAL);
     }
 
-    @AfterEach
-    void clean() {
-        transactionTemplate.executeWithoutResult(
-                status -> em.createQuery("delete WikiPage wp where wp.title=:title and wp.namespace=:namespace")
-                        .setParameter("title", givenTitle.title())
-                        .setParameter("namespace", givenTitle.namespace())
-                        .executeUpdate()
+    private WikiPage getWikiPage(WikiPageTitle givenTitle) {
+        return transactionTemplate.execute(
+                status ->
+                        wikiPageRepository.findByTitleAndNamespace(givenTitle.title(), givenTitle.namespace())
+                                .orElseThrow()
+        );
+    }
+
+    private String getVersionToken(WikiPageTitle givenTitle) {
+        return transactionTemplate.execute(
+                status -> getWikiPage(givenTitle).getVersionToken()
+        );
+    }
+
+    private String getContent(WikiPageTitle givenTitle) {
+        return transactionTemplate.execute(
+                status -> getWikiPage(givenTitle).getContent()
         );
     }
 
     @Test
     @DisplayName("트랜잭션 시점에서의 편집 충돌 테스트")
     void conflict_in_transaction_time() throws InterruptedException {
+        // 무작위 생성 UUID 문서 제목
+        WikiPageTitle givenTitle = new WikiPageTitle(UUID.randomUUID().toString(), Namespace.NORMAL);
+
+        String originalContent = "[[동해물]]과 [[백두산]]이 마르고 닳도록";
+        Set<WikiPageTitle> originalReference = Set.of(normalWikiPageTitle("동해물"), normalWikiPageTitle("백두산"));
+
+        String contentA = "[[동해물]]과 백두산이 마르고 닳도록 [[하느님]]이 보우하사";
+        Set<WikiPageTitle> referenceA = Set.of(normalWikiPageTitle("동해물"), normalWikiPageTitle("하느님"));
+
+        String contentB = "동해물과 [[백두산]]이 마르고 닳도록 하느님이 [[보우]]하사";
+        Set<WikiPageTitle> referenceB = Set.of(normalWikiPageTitle("백두산"), normalWikiPageTitle("보우"));
+
+
+        // 문서 생성 및 초기 값 업데이트
+        wikiPageCommandService.create(UUID.randomUUID(), givenTitle);
+        wikiPageCommandService.commitUpdate(UUID.randomUUID(), givenTitle, randString(), getVersionToken(givenTitle), originalContent, originalReference);
+
+        // 문서 수정을 위한 버전 토큰
+        String givenVersionToken = transactionTemplate.execute(status -> getWikiPage(givenTitle).getVersionToken());
+
+
         UUID contributorA = UUID.randomUUID();
         UUID contributorB = UUID.randomUUID();
-        WikiPageDataForUpdate editDataA = wikiPageCommandService.proclaimUpdate(contributorA, givenTitle);
-        WikiPageDataForUpdate editDataB = wikiPageCommandService.proclaimUpdate(contributorB, givenTitle);
 
-        String contentA = editDataA.content() + randString();
-        String contentB = editDataB.content() + randString();
-
-        EditWorker workerA = EditWorker.builder()
-                .wikiPageCommandService(wikiPageCommandService)
-                .content(contentA)
-                .contributorId(contributorA)
-                .wikiPageDataForUpdate(editDataA)
-                .build();
-
-        EditWorker workerB = EditWorker.builder()
-                .wikiPageCommandService(wikiPageCommandService)
-                .content(contentB)
-                .contributorId(contributorB)
-                .wikiPageDataForUpdate(editDataB)
-                .build();
+        CommitUpdateWorker workerA = createWorker(contentA, givenTitle, givenVersionToken, referenceA, contributorA);
+        CommitUpdateWorker workerB = createWorker(contentB, givenTitle, givenVersionToken, referenceB, contributorB);
 
         Thread threadA = new Thread(workerA);
-        threadA.start();
         Thread threadB = new Thread(workerB);
+
+        threadA.start();
         threadB.start();
 
         threadA.join();
@@ -105,8 +107,8 @@ public class WikiPageCommandServiceEditCollisionTest {
         Exception exceptionA = workerA.getException();
         Exception exceptionB = workerB.getException();
 
-        EditWorker successWorker;
-        EditWorker failWorker;
+        CommitUpdateWorker successWorker;
+        CommitUpdateWorker failWorker;
 
         // 둘 중 하나는 성공해야함
         if (exceptionA == null) {
@@ -119,12 +121,15 @@ public class WikiPageCommandServiceEditCollisionTest {
             failWorker = workerA;
         }
 
-        WikiPageDataForUpdate found = wikiPageCommandService.proclaimUpdate(UUID.randomUUID(), editDataB.titleNamespaceToWikiPageTitle());
-        assertThat(found.content())
-                .isNotEqualTo(editDataA.content())
-                .isEqualTo(successWorker.getContent())
-                .isNotEqualTo(failWorker.getContent())
-        ;
+        String updatedContent = getContent(givenTitle);
+
+        assertThat(updatedContent)
+                .describedAs("수정 이전 content 와 일치해서는 안 됨.")
+                .isNotEqualTo(originalContent)
+                .describedAs("성공한 트랜잭션의 content 반영되어야함.")
+                .isEqualTo(successWorker.getParam().content())
+                .describedAs("실패한 트랜잭션의 content 반영되지 않아야함.")
+                .isNotEqualTo(failWorker.getParam().content());
 
         assertThat(failWorker.getException())
                 .describedAs("락 확보 실패(데드락 포함)로 인한 예외가 아니어야함.")
@@ -134,29 +139,44 @@ public class WikiPageCommandServiceEditCollisionTest {
 
     }
 
+    CommitUpdateWorker createWorker(String content, WikiPageTitle title, String versionToken, Set<WikiPageTitle> reference, UUID contributorId) {
+        CommitUpdateParam commitUpdateParam = new CommitUpdateParam(contributorId, title, randString(), versionToken, content, reference);
+        return CommitUpdateWorker.builder()
+                .wikiPageCommandService(wikiPageCommandService)
+                .param(commitUpdateParam)
+                .build();
+    }
+
+    record CommitUpdateParam(
+            UUID contributorId,
+            WikiPageTitle wikiPageTitle,
+            String comment,
+            String versionToken,
+            String content,
+            Set<WikiPageTitle> reference
+    ) {
+    }
+
+
     @Getter
-    static class EditWorker implements Runnable {
-        private final WikiPageDataForUpdate wikiPageDataForUpdate;
+    static class CommitUpdateWorker implements Runnable {
+        private final CommitUpdateParam param;
         private final WikiPageCommandService wikiPageCommandService;
-        private final UUID contributorId;
-        private final String content;
         private Exception exception;
 
         @Override
         public void run() {
             try {
-                wikiPageCommandService.commitUpdate(contributorId, wikiPageDataForUpdate.titleNamespaceToWikiPageTitle(), randString(), wikiPageDataForUpdate.versionToken(), content, new HashSet<>());
+                wikiPageCommandService.commitUpdate(param.contributorId(), param.wikiPageTitle(), randString(), param.versionToken(), param.content(), param.reference());
             } catch (Exception e) {
                 this.exception = e;
             }
         }
 
         @Builder
-        public EditWorker(WikiPageDataForUpdate wikiPageDataForUpdate, WikiPageCommandService wikiPageCommandService, UUID contributorId, String content) {
-            this.wikiPageDataForUpdate = wikiPageDataForUpdate;
+        public CommitUpdateWorker(CommitUpdateParam param, WikiPageCommandService wikiPageCommandService) {
+            this.param = param;
             this.wikiPageCommandService = wikiPageCommandService;
-            this.contributorId = contributorId;
-            this.content = content;
         }
     }
 
