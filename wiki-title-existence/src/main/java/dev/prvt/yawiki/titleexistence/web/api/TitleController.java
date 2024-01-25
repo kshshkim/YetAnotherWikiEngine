a9d9c39698e2ae84a57d87733e5a0ac95bfaddfd
Seonghyeon Kim <kshshkim@users.noreply.github.com>
2024-01-25 19:43:53 +0900

    refactor: 제목 존재 판별 요청 파라미터 형식 변경
    - url 파라미터로 요청시 파라미터 길이 제한으로 인해 요청에 제한이 생기는 문제를 수정
      - Get 요청의 파라미터를 RequestBody로 받거나, 조회 요청을 Post로 처리하는 것이 표준적인 방법은 아니지만, url 파라미터로 받는 것이 효율적이지 않기 때문에 대안으로 선택함.

--- wiki-title-existence/src/main/java/dev/prvt/yawiki/titleexistence/web/api/TitleController.java
+++ wiki-title-existence/src/main/java/dev/prvt/yawiki/titleexistence/web/api/TitleController.java
@@ -1,13 +1,16 @@
 package dev.prvt.yawiki.titleexistence.web.api;
 
 import dev.prvt.yawiki.common.model.WikiPageTitle;
+import dev.prvt.yawiki.common.webutil.converter.WikiPageTitleConverter;
 import dev.prvt.yawiki.titleexistence.cache.application.WikiPageTitleExistenceFilter;
 import dev.prvt.yawiki.titleexistence.web.api.response.TitleListResponse;
-import java.util.Collection;
+import java.util.List;
+import java.util.stream.Stream;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,20 +18,59 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class TitleController {
 
-    private final WikiPageTitleExistenceFilter titleExistenceFilter;
+    private final WikiPageTitleExistenceFilter filter;
+
+    private final WikiPageTitleConverter wikiPageTitleConverter;
+
+    public record TitleExistenceRequest(
+        List<String> titles
+    ) {
+    }
+
+    private List<WikiPageTitle> convertToWikiPageList(
+        TitleExistenceRequest titleExistenceRequest
+    ) {
+        return titleExistenceRequest.titles().stream()
+                   .map(wikiPageTitleConverter::convert)
+                   .toList();
+    }
 
     /**
-     * 여러 WikiPageTitle 중, 존재하지 않는 제목을 반환함. 위키 문서가 참조중인 문서 제목 대부분은 존재하기 때문에, 존재하지 않는 제목을 반환하는 것이 보다 효율적임.
-     * @param titles "틀:제목" 형태의 문자열 파라미터가 등록된 컨버터를 통해 WikiPageTitle 로 자동 변환됨.
+     * <p>Get 요청에 Body 를 포함시키는 것은 비표준적인 방법이지만, 100개 이상의 파라미터를 url에 포함시키는 것은 굉장히 비효율적이기 때문에 타협함.
+     * <br>ngrinder 등 일부 클라이언트에서 Get 요청 Body 를 지원하지 않는 경우가 있음. 아래의 Post 요청을 활용할 수 있음.</p>
+     *
+     * <p>여러 WikiPageTitle 중, 존재하지 않는 제목을 반환함. 위키 문서가 참조중인 문서 제목 대부분은 존재하기 때문에, 존재하지 않는 제목을 반환하는 것이 보다 효율적임.</p>
+     *
+     * @param request "틀:제목" 형태의 문자열 파라미터. {@link WikiPageTitle} 형태로 자동 변환되지 않음.
      * @return 존재하지 <b>않는</b> 제목 목록 {@link TitleListResponse}
      */
-    @GetMapping("/filter/nonexistent")
-    public TitleListResponse getNonExistTitles(
-            @RequestParam("t") Collection<WikiPageTitle> titles  // 요청 url 이 과도하게 길어지는 것을 방지하기 위해 t 한 글자로 받음.
+    @GetMapping("/nonexistent")
+    public TitleListResponse getNonExistTitlesWithRequestBody(
+        @RequestBody TitleExistenceRequest request
     ) {
         return TitleListResponse.from(
-            titleExistenceFilter.filterExistentTitles(titles)
+            filter.filterExistentTitles(
+                convertToWikiPage(request)
+            )
         );
     }
 
+    /**
+     * <p><b>조회 요청</b>을 POST 메서드로 수행하는 것은 비표준적인 방법이지만, GET 요청으로 100개 이상의 파라미터를 url에 포함시키는 것은 굉장히 비효율적이기 때문에 타협함.</p>
+     *
+     * <p>여러 WikiPageTitle 중, 존재하지 않는 제목을 반환함. 위키 문서가 참조중인 문서 제목 대부분은 존재하기 때문에, 존재하지 않는 제목을 반환하는 것이 보다 효율적임.</p>
+     *
+     * @param request "틀:제목" 형태의 문자열 파라미터. {@link WikiPageTitle} 형태로 자동 변환되지 않음.
+     * @return 존재하지 <b>않는</b> 제목 목록 {@link TitleListResponse}
+     */
+    @PostMapping("/nonexistent")
+    public TitleListResponse getNonExistentTitlesWithPostRequest(
+        @RequestBody TitleExistenceRequest request
+    ) {
+        return TitleListResponse.from(
+            filter.filterExistentTitles(
+                convertToWikiPage(request)
+            )
+        );
+    }
 }
