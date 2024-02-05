ace151ed2ae1a3e987d42226c0fda9ae5b8609dd
Seonghyeon Kim <kshshkim@users.noreply.github.com>
2024-02-05 12:53:48 +0900

    chore: spring boot, spring dependency management 버전 업그레이드
    chore: spring boot 버전 업그레이드
    
    chore: spring boot, spring dependency management 버전 업그레이드

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     java
-    id("org.springframework.boot") version "3.2.1"
-    id("io.spring.dependency-management") version "1.0.15.RELEASE"
+    id("org.springframework.boot") version "3.2.2"
+    id("io.spring.dependency-management") version "1.1.4"
     jacoco
 }
 
